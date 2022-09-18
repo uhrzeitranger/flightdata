4e1560e082815a892feb610b5f51ea5d40c77838
Linus Boll <linusbenedikt.boll@gmail.com>
2022-09-18 17:20:36 +0200

    initial commit

--- ZRHFLO.py
+++ ZRHFLO.py
@@ -1,7 +1,4 @@
-from bs4 import BeautifulSoup
-import requests
-
-url = f'https://www.google.com/flights'
-page_data = requests.get(url)
-page_soup = BeautifulSoup(page_data.content, 'html.parser')
-print(page_soup)
+from selenium import webdriver
+url = 'https://www.google.com/travel/flights/search?tfs=CBwQAhojagwIAhIIL20vMDg5NjYSCjIwMjItMTEtMTFyBwgBEgNGTFIaI2oHCAESA0ZMUhIKMjAyMi0xMS0xM3IMCAISCC9tLzA4OTY2cAGCAQsI____________AUABSAGYAQE'
+driver = webdriver.Chrome()
+driver.get(url)
