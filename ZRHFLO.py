eb0331dcc2666ab94522e169baf4dccf920a607e
Lasse Meixner <68332274+lasse-meixner@users.noreply.github.com>
2022-10-01 18:57:21 +0100

    First script patch (#1)
    * script version
    
    * reorganise
    
    * fix attempt v1
    
    * fixed!
    
    * function update
    
    * no longer breaks for 0 flights
    
    * bump up sleep in location entry
    
    * fix behaviour for unavailable prices
    
    * no longer breaks for expensive flights
    
    * oops
    
    * minor notebook updates
    
    * time & day modifiable, currency automatic
    
    * correction
    
    * allow for next week flights
    
    * improve output message
    
    * currency bug fix
    
    * deleted redundant file
    
    Co-authored-by: Linus Boll <linusbenedikt.boll@gmail.com>

--- ZRHFLO.py
+++
@@ -1,4 +0,0 @@
-from selenium import webdriver
-url = 'https://www.google.com/travel/flights/search?tfs=CBwQAhojagwIAhIIL20vMDg5NjYSCjIwMjItMTEtMTFyBwgBEgNGTFIaI2oHCAESA0ZMUhIKMjAyMi0xMS0xM3IMCAISCC9tLzA4OTY2cAGCAQsI____________AUABSAGYAQE'
-driver = webdriver.Chrome()
-driver.get(url)
